6f0de9b78452f06a8fc912b5f4e973e3166c0563
Ri Ram <eram1294@gmail.com>
2016-02-04 16:43:08 -0500

    Updated tie() checking, added option to pick player symbol and opponent symbol

--- src/Game.java
+++ src/Game.java
@@ -12,36 +12,65 @@ public class Game {
 	
 	// The game board and the game status
 	public static String[] board = {"0", "1", "2", "3", "4", "5", "6", "7", "8"};
+	public static int[][] winConditions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
 	public static int currentState;  // the current state of the game
 	public static String currentPlayer; // the current player
+	public static String playerSymbol;
+	public static String opponentSymbol;
 	
 	public static Scanner input = new Scanner(System.in); // the input Scanner
 	
 	/** The entry main method (the program starts here) */
 	public static void main(String[] args) {
 		// Initialize the game-board and current status
+		pickSymbols();
 		initGame();
 		printBoard();
 		do {
 			getHumanSpot();
+			System.out.println("gameIsOver: " + gameIsOver());
+			System.out.println("tie: " + tie());
 			if (!gameIsOver() && !tie()) {
 				evalBoard();
 			}
 		} while (!gameIsOver() && !tie()); // repeat if not game-over
 		System.out.print("Game over\n");
 	}
+	
+	public static void pickSymbols() {
+		boolean validInput = false;
+		System.out.print("Enter a symbol for the player: ");
+		do {
+			String symbol = input.next();
+			if (symbol.length() == 1) {
+				playerSymbol = symbol;
+				validInput = true;  // input okay, exit loop
+			}
+		} while (!validInput);  // repeat until input is valid
+		
+		validInput = false;
+		
+		System.out.print("Enter a symbol for the opponent: ");
+		do {
+			String symbol = input.next();
+			if (symbol.length() == 1 && !symbol.equals(playerSymbol)) {
+				opponentSymbol = symbol;
+				validInput = true;  // input okay, exit loop
+			}
+		} while (!validInput);  // repeat until input is valid
+	}
 
 	/** Initializes the game */
 	public static void initGame() {
 		currentState = 0; // "playing" or ready to play
-		currentPlayer = "X";  // cross plays first
+		currentPlayer = playerSymbol;  // cross plays first
 	}
 
 	public static String nextPlayer() {
-		if (currentPlayer == "X") {
-			return "O";
+		if (currentPlayer == playerSymbol) {
+			return opponentSymbol;
 		} else {
-			return "X";
+			return playerSymbol;
 		}
 	}
 
@@ -51,8 +80,8 @@ public class Game {
 		System.out.print("Enter [0-8]:\n");
 		do {
 			int spot = input.nextInt();
-			if (board[spot] != "X" && board[spot] != "O") {
-				board[spot] = "X";  // update game-board content
+			if (board[spot] != playerSymbol && board[spot] != opponentSymbol) {
+				board[spot] = playerSymbol;  // update game-board content
 				printBoard();
 				validInput = true;  // input okay, exit loop
 			}
@@ -64,13 +93,13 @@ public class Game {
 		boolean foundSpot = false;
 		do {
 			if (board[4] == "4") {
-				board[4] = "O";
+				board[4] = opponentSymbol;
 				foundSpot = true;
 			} else {
 				int spot = getBestMove();
-				if (board[spot] != "X" && board[spot] != "O") {
+				if (board[spot] != playerSymbol && board[spot] != opponentSymbol) {
 					foundSpot = true;
-					board[spot] = "O";
+					board[spot] = opponentSymbol;
 				} else {
 					foundSpot = false;
 				}
@@ -81,14 +110,18 @@ public class Game {
 
 	/** Return true if the game was just won */
 	public static boolean gameIsOver() {
-		return board[0] == board[1] && board[1] == board[2] ||
-				board[3] == board[4] && board[4] == board[5] ||
-				board[6] == board[7] && board[7] == board[8] ||
-				board[0] == board[3] && board[3] == board[6] ||
-				board[1] == board[4] && board[4] == board[7] ||
-				board[2] == board[5] && board[5] == board[8] ||
-				board[0] == board[4] && board[4] == board[8] ||
-				board[2] == board[4] && board[4] == board[6];
+		for ( int[] w : winConditions )           
+        {
+            if (board[w[0]] == playerSymbol
+                && board[w[1]] == playerSymbol
+                && board[w[2]] == playerSymbol)
+                return true;
+            else if(board[w[0]] == opponentSymbol
+                    && board[w[1]] == opponentSymbol
+                    && board[w[2]] == opponentSymbol)
+            	return true;
+        }
+		return false;
 	}
 
 
@@ -97,19 +130,19 @@ public class Game {
 		boolean foundBestMove = false;
 		int spot = 100;
 		for (String s: board) {
-			if (s != "X" && s != "O") {
+			if (s != playerSymbol && s != opponentSymbol) {
 				availableSpaces.add(s);
 			}
 		}
 		for (String as: availableSpaces) {
 			spot = Integer.parseInt(as);
-			board[spot] = "O";
+			board[spot] = opponentSymbol;
 			if (gameIsOver()) {
 				foundBestMove = true;
 				board[spot] = as;
 				return spot;
 			} else {
-				board[spot] = "X";
+				board[spot] = playerSymbol;
 				if (gameIsOver()) {
 					foundBestMove = true;
 					board[spot] = as;
@@ -128,17 +161,13 @@ public class Game {
 	}
 
 	/** Return true if it is a draw (no more empty cell) */
-	// TODO: maybe there is an easeir way to check this
 	public static boolean tie() {
-		return board[0] != "0" &&
-				board[1] != "1" &&
-				board[2] != "2" &&
-				board[3] != "3" &&
-				board[4] != "4" &&
-				board[5] != "5" &&
-				board[6] != "6" &&
-				board[7] != "7" &&
-				board[8] != "8";
+		for(String b : board)
+		{
+			if(!b.equals(playerSymbol) && !b.equals(opponentSymbol))
+				return false;
+		}
+		return true;
 	}
 
 	/** Print the game board */
